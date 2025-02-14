1e91f98ead7c132bac99acb914cf61225c19e3e3
hjzheld <dusen7774@naver.com>
2025-02-15 03:35:16 +0900

    Add: 헤더 및 푸터 추가 / 장고 템플릿 연결 중중

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,20 @@
 <template>
   <div>
+    <Header />
     <router-view></router-view>
+    <Footer />
   </div>
 </template>
 
 <script setup>
+import Header from "@/components/layout/Header.vue"
+import Footer from "@/components/layout/Footer.vue"
 
 </script>
 
-<style></style>
+<style>
+@import '@/assets/scss/_style.scss';
+@import '@/assets/scss/setting.scss';
+@import '@/assets/scss/layout/header.scss';
+@import '@/assets/scss/layout/footer.scss';
+</style>
---
+++ frontend/src/components/layout/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="header-container">
+        <div class="header-title">
+            <h2>호진일보</h2>
+            <p>연예 일간랭킹</p>
+        </div>
+        <p>* 매일 오후 8시 기준</p>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+
+</style>
