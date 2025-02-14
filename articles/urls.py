c666f8c8ba70aa5c0907dc51ad1bbbf05ed5df7d
gihohoho <leekh9997@naver.com>
2025-02-14 22:31:22 +0900

    Add: article url 추가

--- articles/urls.py
+++ articles/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.ArticleView.as_view(), name="article_view"),
+    path(
+        "<int:article_id>/",
+        views.ArticleDetailView.as_view(),
+        name="article_detail_view",
+    ),
+]
