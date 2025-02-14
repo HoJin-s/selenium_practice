9d46f71287dfc9bada6d90cf6e88aa128c3a00ce
gihohoho <leekh9997@naver.com>
2025-02-14 16:21:38 +0900

    Fix: 영어요일을 한글요일로 바꿈

--- articles/models.py
+++ articles/models.py
@@ -10,9 +10,25 @@ class Article(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     day_of_week_category = models.CharField(max_length=20, editable=False)  # 요일 저장
 
+    def _change_day(self, day):
+        days = {
+            "Monday": "월요일",
+            "Tuesday": "화요일",
+            "Wednesday": "수요일",
+            "Thursday": "목요일",
+            "Friday": "금요일",
+            "Saturday": "토요일",
+            "Sunday": "일요일",
+        }
+
+        return days[day]
+
     def save(self, *args, **kwargs):
         if not self.pk:
             self.day_of_week_category = localtime().strftime("%A")
+
+        self.day_of_week_category = self._change_day(self.day_of_week_category)
+
         super().save(*args, **kwargs)
 
     def __str__(self):
