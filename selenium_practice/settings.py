6b19f02f5cd8af694fd328f00e20bc0b6fd2a2ef
gihohoho <leekh9997@naver.com>
2025-02-14 16:08:26 +0900

    Add: media/static 파일 추가

--- selenium_practice/settings.py
+++ selenium_practice/settings.py
@@ -119,7 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
+STATIC_URL = "/static/"
+
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
