ded24ea721903c9d8e0b0f2f2ffbe732c790e1b0
gihohoho <leekh9997@naver.com>
2025-02-14 18:16:42 +0900

    Fix: 이미지필드 삭제(content에 리스트로 이미지url 추가)

--- articles/models.py
+++ articles/models.py
@@ -6,7 +6,6 @@ from django.utils.timezone import localtime
 class Article(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    image = models.URLField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     day_of_week_category = models.CharField(max_length=20, editable=False)  # 요일 저장
 
